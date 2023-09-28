73b039b90be3b3049bb7bf2fc9c3511cf997222e
David Sarabia <david.sarabia@i2cat.net>
2023-09-28 16:25:02 +0200

    Updating setup

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ import setuptools
 setuptools.setup(
     name='rina',
     version='0.0.3',
+    license= 'GNU General Public License version 3',
     author='David Sarabia',
     author_email='david.sarabia@i2cat.net',
     description='rina driver for implementing SDN controller',
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Licence :: OSI Approved :: MIT Licencse",
+        "Licence :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent",
 
     ],
